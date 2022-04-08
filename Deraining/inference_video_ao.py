01fd526517ba2cc83b3ca169ab40ff55cacf782d
ZhangAoCanada <zhangaocanada@gmail.com>
2022-04-08 13:56:47 +0800

    inference videos

--- Deraining/inference_video_ao.py
+++ Deraining/inference_video_ao.py
@@ -30,13 +30,13 @@ model_restoration.to(device)
 model_restoration = nn.DataParallel(model_restoration)
 model_restoration.eval()
 
-video_saving_dir = "./checkpoints/Deraining/videos/MPRNet/"
+video_saving_dir = "./checkpoints/Deraining_2070images/videos/MPRNet/"
 utils.mkdir(video_saving_dir)
 
-video_path = "/content/drive/MyDrive/DERAIN/DATA_captured/something_else/dusty_video1.mp4"
-output_video_path = os.path.join(video_saving_dir, "dusty_video1_result.avi")
-# video_path = "/content/drive/MyDrive/DERAIN/DATA_captured/something_else/dusty_water_video1.mp4"
-# output_video_path = os.path.join(video_saving_dir, "dusty_water_video1_result.avi")
+# video_path = "/content/drive/MyDrive/DERAIN/DATA_captured/something_else/dusty_video1.mp4"
+# output_video_path = os.path.join(video_saving_dir, "dusty_video1_result.avi")
+video_path = "/content/drive/MyDrive/DERAIN/DATA_captured/something_else/dusty_water_video1.mp4"
+output_video_path = os.path.join(video_saving_dir, "dusty_water_video1_result.avi")
 # video_path = "/content/drive/MyDrive/DERAIN/DATA_captured/something_else/sample_video1.mp4"
 # output_video_path = os.path.join(video_saving_dir, "result_video1.avi")
 
